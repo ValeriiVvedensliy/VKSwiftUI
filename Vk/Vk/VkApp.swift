f9214cac6381c4fd66b0698ccf231b316f5f0b20
Valerii <valeronmaestro@gmail.com>
2022-01-13 19:47:20 +0200

    created-project

---
+++ Vk/Vk/VkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VkApp.swift
+//  Vk
+//
+//  Created by Valera Vvedenskiy on 13.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct VkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
